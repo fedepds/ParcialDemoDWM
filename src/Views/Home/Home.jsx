fe9c42497c5d46e74eec6ba1f9636abc0e28dd64
Federico Pizarro <127800142+fedepds@users.noreply.github.com>
2024-10-16 17:46:22 -0300

    agregue filtro

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -12,19 +12,34 @@ const getGames = async () => {
 
 // Componente principal Home
 const Home = () => {
-  const [games, setGames] = useState([]); // Define el estado 'games' y su setter 'setGames', inicializado como un array vacío
+  const [games, setGames] = useState([]); // Define el estado 'games' para almacenar los juegos obtenidos
+  const [searchTerm, setSearchTerm] = useState(""); // Estado para almacenar el término de búsqueda
+  const [filteredGames, setFilteredGames] = useState([]); // Estado para almacenar los juegos filtrados
   const navigate = useNavigate(); // Hook para redirigir al usuario a diferentes rutas
 
   // Función para refrescar la lista de juegos obteniendo los más recientes desde la API
   const refreshGames = async () => {
     const updatedGames = await getGames(); // Obtiene los juegos actualizados
     setGames(updatedGames); // Actualiza el estado con los nuevos juegos
+    setFilteredGames(updatedGames); // Inicializa los juegos filtrados con todos los juegos
   };
 
   // Hook useEffect que se ejecuta una vez al cargar el componente para obtener los juegos iniciales
   useEffect(() => {
     refreshGames(); // Llama a refreshGames cuando el componente se monta
   }, []); // Se ejecuta solo una vez, ya que el array de dependencias está vacío
+
+  // Función para manejar el filtro de búsqueda
+  const handleSearch = (event) => {
+    const searchValue = event.target.value.toLowerCase(); // Convierte el término de búsqueda a minúsculas
+    setSearchTerm(searchValue); // Actualiza el estado del término de búsqueda
+
+    // Filtra los juegos basados en el título
+    const filtered = games.filter((game) =>
+      game.title.toLowerCase().includes(searchValue)
+    );
+    setFilteredGames(filtered); // Actualiza el estado con los juegos filtrados
+  };
 
   // Función para manejar el click en el botón "Agregar Juego", redirige a la página de agregar juegos
   const handleAddGameClick = () => {
@@ -39,9 +54,19 @@ const Home = () => {
           Agregar Juego {/* Botón para agregar un juego */}
         </button>
       </div>
-      {games.length ? ( // Si hay juegos disponibles, los muestra en formato de tarjetas
+
+      {/* Input de búsqueda */}
+      <input
+        type="text"
+        placeholder="Buscar por nombre"
+        value={searchTerm}
+        onChange={handleSearch}
+        className="search-input"
+      />
+
+      {filteredGames.length ? ( // Si hay juegos filtrados disponibles, los muestra en formato de tarjetas
         <div className="home-grid-cards">
-          {games.map((game) => (
+          {filteredGames.map((game) => (
             <Card
               key={game.id} // Se asigna una key única a cada tarjeta basada en el id del juego
               title={game.title} // Pasa el título del juego como prop
